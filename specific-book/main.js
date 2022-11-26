a5d7507517357f51da03419af86fbf49b2ecc56b
Eduard Konovka <ed098ua@gmail.com>
2022-11-27 00:55:34 +0200

    Chapter 8 (JS Basics)

--- specific-book/main.js
+++ specific-book/main.js
@@ -1,9 +1,14 @@
-const handleSelect = (e) => {
-  const obj = {
-    _id,
-    qwantity: e.target.value,
-    cost: price * e.target.value,
-  };
+const refs = {
+  price: document.querySelector('[data-name="price"]'),
+  count: document.querySelector('input[name="count"]'),
+  totalPrice: document.querySelector('[data-name="totalPrice"]'),
+  form: document.querySelector("form"),
+};
 
-  onSelectQwantity(obj);
+refs.form.onchange = (e) => {
+  e.target.value > 1 && e.target.value <= 42
+    ? (refs.totalPrice.textContent = (
+        Number(refs.price.textContent) * e.target.value
+      ).toFixed(2))
+    : (refs.count.value = 1) && alert("Please enter a value between 0 and 43!");
 };
