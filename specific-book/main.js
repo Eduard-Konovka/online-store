cfbf94afa8f1e601974d07fa02f969019ca8b8e8
Eduard Konovka <ed098ua@gmail.com>
2022-11-26 23:54:45 +0200

    Adds handleSelect for input

--- specific-book/main.js
+++ specific-book/main.js
@@ -1,1 +1,9 @@
-console.log("Connected");
+const handleSelect = (e) => {
+  const obj = {
+    _id,
+    qwantity: e.target.value,
+    cost: price * e.target.value,
+  };
+
+  onSelectQwantity(obj);
+};
