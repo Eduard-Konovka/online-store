0c060203a92c687cb09dca485e88ca081e4aa92d
Eduard Konovka <ed098ua@gmail.com>
2022-12-30 23:38:26 +0200

    Adds BooksView

--- src/pages/BooksView/BooksView.jsx
+++ src/pages/BooksView/BooksView.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import ShopsBar from 'components/ShopsBar';
-import ProductsBar from 'components/ProductsBar';
+import fetchProducts from 'api/productsApi';
+import Spinner from 'components/Spinner';
+import ProductsList from 'components/ProductsList';
+import Blank from 'components/Blank';
+import imageBlank from 'images/shop.jpg';
 import s from './BooksView.module.css';
 
 export default function BooksView({ onClick }) {
   const [shopId, setShopId] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetchProducts()
+      .then(products => {
+        setProducts(products);
+      })
+      .catch(error => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
-    <div className={s.shoppage}>
-      <ShopsBar onClick={id => setShopId(id)} />
-      <ProductsBar shopId={shopId} onClick={onClick} />
+    <div className={s.bookpage}>
+      {error && <p>Whoops, something went wrong: {error.message}</p>}
+      {loading && <Spinner size={70} color="blue" />}
+      {!loading && shopId === null && (
+        <Blank title="Choose a shop" image={imageBlank} alt="Open shop" />
+      )}
+      {products.length > 0 && (
+        <ProductsList products={products} shopId={shopId} onClick={onClick} />
+      )}
     </div>
   );
 }
 
-ProductsBar.propTypes = {
+BooksView.propTypes = {
   onClick: PropTypes.func.isRequired,
 };
 
