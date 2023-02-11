0bcced5201617dff07877c4f604fd9731dd4682a
Eduard Konovka <ed098ua@gmail.com>
2023-02-12 00:14:17 +0200

    Fix tests

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as BooksView } from './BooksView';
+export { default as CartView } from './CartView';
+export { default as NotFoundView } from './NotFoundView';
+export { default as SignInView } from './SignInView';
+export { default as SpecificBookView } from './SpecificBookView';
