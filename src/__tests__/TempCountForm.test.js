132afac698f3bf1bba1681949023d87235eb4fc4
Eduard Konovka <ed098ua@gmail.com>
2024-02-22 18:04:03 +0200

    Adds TempCountForm

---
+++ src/__tests__/TempCountForm.test.js
@@ -0,0 +1,161 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import TempCountForm from '../components/CountForm/TempCountForm';
+
+const COUNT = 3;
+const PRICE = 7.77;
+const MIN = 1;
+const MAX = 42;
+const STYLES = {
+  formStyle: 'count-form-style',
+  labelStyle: 'label-style',
+  inputStyle: 'input-style',
+  spanStyle: 'span-style',
+  totalPriceStyle: 'total-price-style',
+};
+let returnedCountValue;
+
+const mockComponent = (
+  <TempCountForm
+    value={COUNT}
+    price={PRICE}
+    min={MIN}
+    max={MAX}
+    styles={STYLES}
+    setCount={count => (returnedCountValue = count)}
+  />
+);
+
+describe('Testing CountForm component', () => {
+  test('1. Title of lable', () => {
+    render(mockComponent);
+
+    expect(screen.getByLabelText('Count, units:')).toBeInTheDocument();
+  });
+
+  test('2. Title of Total price', () => {
+    render(mockComponent);
+
+    expect(screen.getByText('Total price:')).toBeInTheDocument();
+  });
+
+  test('3. Input with type "number"', () => {
+    render(mockComponent);
+
+    expect(screen.getByRole('spinbutton')).toBeInTheDocument();
+  });
+
+  // test(`4. Counter with a given initial value of ${COUNT}`, () => {
+  //   render(mockComponent);
+
+  //   expect(screen.getByDisplayValue(COUNT)).toBeInTheDocument();
+  // });
+
+  test('5. Value of Total price', () => {
+    render(mockComponent);
+    const regexp = new RegExp(`${(COUNT * PRICE).toFixed(2)}`);
+
+    expect(screen.getByText(regexp)).toBeInTheDocument();
+  });
+
+  test(`6. Count input with value incremented by 1`, async () => {
+    render(mockComponent);
+
+    const input = screen.getByRole('spinbutton');
+
+    fireEvent.change(input, {
+      target: { value: COUNT + 1 },
+    });
+
+    expect(returnedCountValue).toBe(COUNT + 1);
+  });
+
+  test(`7. Count input with value reduced by 1`, async () => {
+    render(mockComponent);
+
+    const input = screen.getByRole('spinbutton');
+
+    fireEvent.change(input, {
+      target: { value: COUNT - 1 },
+    });
+
+    expect(returnedCountValue).toBe(COUNT - 1);
+  });
+
+  test(`8. The quantity entered by the user is incremented by 1`, async () => {
+    const user = userEvent.setup();
+
+    render(mockComponent);
+
+    const input = screen.getByRole('spinbutton', {
+      name: /count, units:/i,
+    });
+
+    await user.type(input, (COUNT + 1).toString(), {
+      initialSelectionStart: 0,
+      initialSelectionEnd: 1,
+    });
+
+    expect(returnedCountValue).toBe(COUNT + 1);
+  });
+
+  test(`9. The quantity entered by the user is reduced by 1`, async () => {
+    const user = userEvent.setup();
+
+    render(mockComponent);
+
+    const input = screen.getByLabelText('Count, units:');
+
+    await user.type(input, (COUNT - 1).toString(), {
+      initialSelectionStart: 0,
+      initialSelectionEnd: 1,
+    });
+
+    expect(returnedCountValue).toBe(COUNT - 1);
+  });
+
+  test(`10. One digit is added to the amount entered by the user`, async () => {
+    const user = userEvent.setup();
+
+    render(mockComponent);
+
+    const input = screen.getByLabelText('Count, units:');
+
+    await user.type(input, '1');
+
+    expect(returnedCountValue).toBe(Number(COUNT.toString() + '1'));
+  });
+
+  test(`Temp 1. The quantity entered by the user is incremented by 1`, async () => {
+    const user = userEvent.setup();
+
+    render(mockComponent);
+
+    const input = screen.getByRole('textbox', {
+      name: /text count:/i,
+    });
+
+    await user.type(input, (COUNT + 1).toString(), {
+      initialSelectionStart: 2,
+      initialSelectionEnd: 0,
+    });
+
+    expect(returnedCountValue).toBe(COUNT + 1);
+  });
+
+  test(`Temp 2. The quantity entered by the user is reduced by 1`, async () => {
+    const user = userEvent.setup();
+
+    render(mockComponent);
+
+    const input = screen.getByLabelText('Text Count:');
+
+    await user.type(input, (COUNT - 1).toString(), {
+      initialSelectionStart: 2,
+      initialSelectionEnd: 0,
+    });
+
+    expect(returnedCountValue).toBe(COUNT - 1);
+  });
+});
---
+++ src/components/CountForm/TempCountForm.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { GLOBAL } from '../../constants';
+
+export default function TempCountForm({
+  value,
+  price,
+  min = 1,
+  max = Infinity,
+  styles,
+  setCount,
+}) {
+  const [totalPrice, setTotalPrice] = useState(price);
+
+  useEffect(() => {
+    setTotalPrice((Number(price) * Number(value)).toFixed(2));
+  }, [value, price]);
+
+  function handleKeyPress(event) {
+    if (
+      GLOBAL.keyСodes.prohibited.includes(event.charCode) ||
+      (event.charCode === GLOBAL.keyСodes.zero && !event.target.value)
+    ) {
+      event.preventDefault();
+    }
+  }
+
+  function handleChange(event) {
+    const inputValue = Number(event.target.value);
+
+    if (
+      inputValue >= min &&
+      inputValue <= max &&
+      Number.isInteger(inputValue)
+    ) {
+      setCount(inputValue);
+    } else {
+      toast.error(
+        `Please enter an integer value from ${min} to ${max} inclusive!`,
+      );
+    }
+  }
+
+  return (
+    <>
+      <form className={styles.formStyle}>
+        <label htmlFor="count" className={styles.labelStyle}>
+          Count, units:
+        </label>
+
+        <input
+          name="count"
+          id="count"
+          type="number"
+          min={min}
+          max={max}
+          value={value > 0 ? value : ''}
+          className={styles.inputStyle}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="textCount" className={styles.labelStyle}>
+          Text Count:
+        </label>
+
+        <input
+          name="textCount"
+          id="textCount"
+          type="text"
+          value={value > 0 ? Number(value) : ''}
+          className={styles.inputStyle}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+        />
+      </form>
+
+      <p className={styles.totalPriceStyle}>
+        <span className={styles.totalPriceTitleStyle}>Total price: </span>
+        <span className={styles.totalPriceValueStyle}>${totalPrice}</span>
+      </p>
+    </>
+  );
+}
+
+TempCountForm.propTypes = {
+  value: PropTypes.number.isRequired,
+  price: PropTypes.number,
+  min: PropTypes.number,
+  max: PropTypes.number,
+  styles: PropTypes.shape({
+    formStyle: PropTypes.string,
+    labelStyle: PropTypes.string,
+    inputStyle: PropTypes.string,
+    totalPriceStyle: PropTypes.string,
+    totalPriceTitleStyle: PropTypes.string,
+    totalPriceValueStyle: PropTypes.string,
+  }).isRequired,
+  setCount: PropTypes.func.isRequired,
+};
