f5e145ba18e05863102df647818e921d6e939b10
Eduard Konovka <ed098ua@gmail.com>
2024-03-25 23:45:40 +0200

    Fixes Spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,11 +1,15 @@
-import { ImSpinner } from 'react-icons/im';
+import { FcSettings } from 'react-icons/fc';
+import { IoMdCog } from 'react-icons/io';
+import { AiFillSetting } from 'react-icons/ai';
 import PropTypes from 'prop-types';
 import s from './Spinner.module.css';
 
 export default function Spinner({ size = 50, color = 'black' }) {
   return (
     <div className={s.box}>
-      <ImSpinner size={size} className={s[color]} alt="Spinner" />
+      <FcSettings size={size} className={s.spinner} alt="Spinner" />
+      <IoMdCog size={size * 1.6} className={s[color]} alt="Spinner" />
+      <AiFillSetting size={size * 0.6} className={s.gray} alt="Spinner" />
     </div>
   );
 }
